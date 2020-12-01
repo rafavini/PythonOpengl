f6940ca5568c0e0cfb27091916842a88355b72de
rafavini <42884321+rafavini@users.noreply.github.com>
2020-11-30 20:32:29 -0400

    Update trabalho.py

--- trabalho.py
+++ trabalho.py
@@ -74,15 +74,9 @@ def init():
 	scale = pyrr.matrix44.create_from_scale([0.5,0.5,0.5],dtype='f')
 	model = pyrr.matrix44.multiply(model,scale)
 
-	if(rotate == 1):
-		rotZ = pyrr.matrix44.create_from_z_rotation(math.radians(45))
-		rotY = pyrr.matrix44.create_from_y_rotation(math.radians(45))
-		rotx = pyrr.matrix44.create_from_x_rotation(math.radians(45))
-		rotT = pyrr.matrix44.multiply(rotY,rotx)
-		rotT = pyrr.matrix44.multiply(rotT,rotZ)
-
-		model = pyrr.matrix44.multiply(model,rotT)
-		# atribui uma variavel uniforme para matriz de transformacao
+	
+	
+	# atribui uma variavel uniforme para matriz de transformacao
 	uMat = glGetUniformLocation(shaderProgram, "model")
 
 	
@@ -95,52 +89,13 @@ def init():
 
 
 def rotate():
-	global shaderProgram
-	global vao
-	global vbo
 	global model
 	global uMat
-	
-	
-
-
-	glClearColor(0, 0, 0, 1)
-	
-	vertex_code = readShaderFile('cube.vp')
-	fragment_code = readShaderFile('cube.fp')
-
-	# compile shaders and program
-	vertexShader = shaders.compileShader(vertex_code, GL_VERTEX_SHADER)
-	fragmentShader = shaders.compileShader(fragment_code, GL_FRAGMENT_SHADER)
-	shaderProgram = shaders.compileProgram(vertexShader, fragmentShader)
-	
-	# Create and bind the Vertex Array Object
-	vao = GLuint(0)
-	glGenVertexArrays(1, vao)
-	glBindVertexArray(vao)
-	
-
-    # lendo os obj pegando vertices e normais
-	vertices = np.array(readVertexData(), dtype='f')
-	print("vertices:", len(vertices)//6)
-	print(vertices)
 
 	
-	vbo = glGenBuffers(1)
-	glBindBuffer(GL_ARRAY_BUFFER, vbo)
-	glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
-	glVertexAttribPointer(0, 3, GL_FLOAT, False, 6 * sizeof(GLfloat), ctypes.c_void_p(3*sizeof(GLfloat)))  # first 0 is the location in shader
-	glVertexAttribPointer(1, 3, GL_FLOAT, False, 6 * sizeof(GLfloat), ctypes.c_void_p(0))  # first 0 is the location in shader
-	#glVertexAttribPointer(0, 3, GL_FLOAT, False, 0, None)  # first 0 is the location in shader
-	#glBindAttribLocation(shaderProgram, 0, 'vertexPosition')  # name of attribute in shader
-	glEnableVertexAttribArray(0)  # 0=location do atributo, tem que ativar todos os atributos inicialmente sao desabilitados por padrao
-	glEnableVertexAttribArray(1)  # 0=location do atributo, tem que ativar todos os atributos inicialmente sao desabilitados por padrao
-	# cria a matriz de transformação
 	model = pyrr.matrix44.create_identity()
-	scale = pyrr.matrix44.create_from_scale([0.5,0.5,0.5],dtype='f')
-	model = pyrr.matrix44.multiply(model,scale)
 
-	rotZ = pyrr.matrix44.create_from_z_rotation(math.radians(45))
+	rotZ = pyrr.matrix44.create_from_z_rotation(math.radians(0))
 	rotY = pyrr.matrix44.create_from_y_rotation(math.radians(45))
 	rotx = pyrr.matrix44.create_from_x_rotation(math.radians(45))
 	rotT = pyrr.matrix44.multiply(rotY,rotx)
@@ -149,12 +104,6 @@ def rotate():
 	model = pyrr.matrix44.multiply(model,rotT)
 	# atribui uma variavel uniforme para matriz de transformacao
 	uMat = glGetUniformLocation(shaderProgram, "model")
-
-	# Note that this is allowed, the call to glVertexAttribPointer registered VBO
-	# as the currently bound vertex buffer object so afterwards we can safely unbind
-	glBindBuffer(GL_ARRAY_BUFFER, 0)
-	# Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
-	glBindVertexArray(0)
 
 def display():
 	global shaderProgram
@@ -167,29 +116,31 @@ def display():
 	glUseProgram(shaderProgram)
 	glBindVertexArray(vao)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo)
-	Color = glGetUniformLocation(shaderProgram,"uColor")
 	glUniformMatrix4fv(uMat, 1, GL_FALSE, model)
 	#glDrawArrays( mode , first, count)
 	#glDrawArrays(GL_LINES, 0, 36)
-	texto = 'casa'
-	while(texto != 'quit'):
-		texto = input()
+	arq = sys.argv[1]
 
-		if(texto == 'rotate'):
-			
+	arquivo = open(arq,'r')
+	for linha in arquivo:
+		linha = linha.replace('\n','')
+		if(linha == 'add'):
+			glDrawArrays(GL_TRIANGLES, 0, 36)
+		
+		if(linha == 'rotate'):
+			rotate()
 			glDrawArrays(GL_TRIANGLES, 0, 36)
 
-		if(texto == 'add'):
-			glDrawArrays(GL_TRIANGLES, 0, 3462)
 
-		
+
+		print(linha)
 
 		#clean things up
-		glBindBuffer(GL_ARRAY_BUFFER, 0)
-		glBindVertexArray(0)
-		glUseProgram(0)
+	glBindBuffer(GL_ARRAY_BUFFER, 0)
+	glBindVertexArray(0)
+	glUseProgram(0)
 	
-		glutSwapBuffers()  # necessario para windows!
+	glutSwapBuffers()  # necessario para windows!
 
 	
 	
@@ -210,6 +161,7 @@ if __name__ == '__main__':
 	
 	glutReshapeFunc(reshape)
 	glutDisplayFunc(display)
+	glutIdleFunc(display)
 	
 
 	init()
